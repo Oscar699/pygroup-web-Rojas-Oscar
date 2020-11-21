ab3881637a717dbecc1aaa84e2a18dfc833c0f11
Oscar699 <52666582+Oscar699@users.noreply.github.com>
2020-11-20 22:17:16 -0500

    Contiene los formularios de la practica y de la tarea formularios

---
+++ proyecto_flask/app/products/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField
+from wtforms.validators import DataRequired, Length, InputRequired
+
+
+class CreateCategoryForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+
+class CreateProductForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=50, message="The name is to long")])
+    price = IntegerField('Price', validators=[DataRequired()])
+    weight = IntegerField('Weight', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired(), Length(max=500, message="The description is to long")])
+    refundable = SelectField('Refundable', choices=[(True, 'Yes'), (False, 'No')], validators=[InputRequired()], coerce=lambda x: x == 'True')
+    category_id = IntegerField('Category id', validators=[DataRequired()])
