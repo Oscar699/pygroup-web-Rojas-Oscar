6a8013a9a167a147dc345b3683510682bb7fced4
Oscar699 <52666582+Oscar699@users.noreply.github.com>
2020-11-22 16:10:18 -0500

    Cambios para solucionar error de valores "refundable"

--- proyecto_flask/app/products/forms.py
+++ proyecto_flask/app/products/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField
+from wtforms import StringField, IntegerField, SelectField, RadioField
 from wtforms.validators import DataRequired, Length, InputRequired
 
 
@@ -12,5 +12,5 @@ class CreateProductForm(FlaskForm):
     price = IntegerField('Price', validators=[DataRequired()])
     weight = IntegerField('Weight', validators=[DataRequired()])
     description = StringField('Description', validators=[DataRequired(), Length(max=500, message="The description is to long")])
-    refundable = SelectField('Refundable', choices=[(True, 'Yes'), (False, 'No')], validators=[InputRequired()], coerce=lambda x: x == 'True')
+    refundable = SelectField('Refundable', choices=[("True", 'Yes'), ("", 'No')], coerce=bool)
     category_id = IntegerField('Category id', validators=[DataRequired()])
