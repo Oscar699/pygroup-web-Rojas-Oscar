1f68ba9b80632c3d5aa9948eb2d7048ac2372a93
Oscar699 <52666582+Oscar699@users.noreply.github.com>
2021-02-14 09:47:45 -0500

    Se añaden login_manager y migrate.

--- proyecto_final/app/__init__.py
+++ proyecto_final/app/__init__.py
@@ -1,19 +1,28 @@
 from flask import Flask
 
 from app.db import db, ma
+from flask_login import LoginManager
 from conf.config import DevelopmentConfig
 from app.products.views import store
+from app.clients.views import userBp
+from app.clients.models import User
+from flask_migrate import Migrate
 
-ACTIVE_ENDPOINTS = [('/store', store)]
+
+ACTIVE_ENDPOINTS = [('/store', store), ('/user', userBp)]
 
 
 def create_app(config=DevelopmentConfig):
     app = Flask(__name__)
 
     app.config.from_object(config)
+    migrate = Migrate()
+    login_manager = LoginManager()
 
+    login_manager.init_app(app)
     db.init_app(app)
     ma.init_app(app)
+    migrate.init_app(app, db=db)
 
     with app.app_context():
         db.create_all()
@@ -21,6 +30,12 @@ def create_app(config=DevelopmentConfig):
     # register each active blueprint
     for url, blueprint in ACTIVE_ENDPOINTS:
         app.register_blueprint(blueprint, url_prefix=url)
+
+    login_manager.login_view = "userBp.login"
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
 
